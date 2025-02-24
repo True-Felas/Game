fb67ac7addf9f521dacf38055a96777c84edaffa
Luis-matador <angelluismatadorposada@gmail.com>
2025-02-24 16:22:34 +0100

    Add files via upload

---
+++ ProyectoJava/src/Pantalla.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Pantalla extends JFrame {
+
+
+    public Pantalla() {
+        setTitle("Pantalla");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+
+
+    }
+}
