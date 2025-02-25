a56e8c080efc5fcb208cda3d2ae17c92d025ee2a
True-Felas <juan.jimenez.nieto1@gmail.com>
2025-02-25 16:57:52 +0100

    Movimiento y añadir movimiento a pantalla creado, de forma provisional.

--- ProyectoJava/src/Pantalla.java
+++ ProyectoJava/src/Pantalla.java
@@ -5,13 +5,17 @@ public class Pantalla extends JFrame {
 
     public Pantalla() {
         setTitle("Pantalla");
-        setSize(800, 600);
+        setSize(1280, 720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
 
+        Movimiento movimiento = new Movimiento();
+        add(movimiento);
 
+        setVisible(true);
+        movimiento.requestFocusInWindow();
 
     }
 }
